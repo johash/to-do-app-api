acc5293cce58e1aa5a09f9a40a1e01229a3af362
johash <johashfiscal@gmail.com>
2022-07-22 08:48:35 +0530

    allow cors

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ mongoose.connect('mongodb+srv://johash:johash529440@to-do-database.iqyhvan.mongo
 
 const todosRoutes = require("./api/routes/todos");
 
+app.use((req, res) => {
+  res.header('Access-Control-Allow_Origin', '*'),
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({})
+  }
+})
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use("/api/todos", todosRoutes);
